7102cd1402b020946406526a66da4863f4d3fe10
Fumiya <i.make.a.studious.effort@gmail.com>
2023-07-31 10:21:25 +0900

    distの変更

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -28,7 +28,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const stocker_1 = __importDefault(require("./routes/stocker"));
-const path_1 = __importDefault(require("path"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv = __importStar(require("dotenv"));
@@ -53,10 +52,12 @@ app.use((0, cors_1.default)(options));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, body_parser_1.json)());
 app.use(express_1.default.static('public'));
-app.use(express_1.default.static(path_1.default.join(__dirname, '../..', 'frontend/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path_1.default.resolve(__dirname, '../..', 'frontend/build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../..', 'frontend/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(
+//     path.resolve(__dirname, '../..', 'frontend/build', 'index.html')
+//   );
+// });
 app.use('/stocker', stocker_1.default);
 app.use('*', (err, req, res, next) => {
     if (!err.message) {
