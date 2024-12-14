0fd8c5a3d9da51780d9999113a2f99ac35db1551
north238 <i.make.a.studious.effort@gmail.com>
2024-12-15 06:50:33 +0900

    検索コンポーネント配置

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import ProfileMenu from './mui/ProfileMenu';
+import SearchArea from './mui/SearchArea';
 import { useAuthContext } from 'contexts/AuthContext';
 
 const Header: React.FC = () => {
   const { user } = useAuthContext();
 
   return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography
+          variant="h6"
+          component="a"
+          href="/"
+          sx={{
+            flexGrow: 1,
+            textDecoration: 'none',
+            color: 'inherit',
+            ml: '10px',
+          }}
+        >
           うちStock
         </Typography>
-        <Button color="inherit" component={RouterLink} to="/">
-          ホーム
-        </Button>
+        <SearchArea />
         {user ? (
           <ProfileMenu />
         ) : (
