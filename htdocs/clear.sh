ee87ef32dd290e4feff21283cb9bbd64c3870b8b
north238 <i.make.a.studious.effort@gmail.com>
2025-03-16 07:04:51 +0900

    react導入

---
+++ htdocs/clear.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+php artiasn cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
--- htdocs/vite.config.js
+++ htdocs/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        host: "localhost",
+        port: 5173,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: "resources/js/app.tsx",
+            ssr: "resources/js/ssr.tsx",
             refresh: true,
         }),
+        react(),
     ],
 });
