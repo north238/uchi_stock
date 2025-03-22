7ed4643a14fbf049c95062ecc14cb28d8025a74d
north238 <i.make.a.studious.effort@gmail.com>
2025-03-22 17:08:06 +0900

    LINEログインボタン設置

---
+++ htdocs/resources/js/Components/LineLogin.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes } from "react";
+
+export default function LineLogin({
+    className = "",
+    disabled,
+    children,
+    ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+    return (
+        <button
+            {...props}
+            className={
+                `inline-flex justify-center gap-1 w-full p-2 bg-LINE01 text-white font-bold rounded-md ${
+                    disabled && "opacity-25"
+                } ` + className
+            }
+            disabled={disabled}
+        >
+            <img src="/icon/icons8-line.svg" alt="LINE LOGO" />
+            {children}
+        </button>
+    );
+}
