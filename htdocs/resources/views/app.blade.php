5283b3f582cabac7c2ee4603e60147a814d95062
north238 <i.make.a.studious.effort@gmail.com>
2025-11-27 04:48:08 +0900

    改修：ファビコン設置

--- htdocs/resources/views/app.blade.php
+++ htdocs/resources/views/app.blade.php
@@ -7,7 +7,12 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="shortcut icon" href="{{ asset('img/Favicon2_16x16.svg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}" />
+    <meta name="apple-mobile-web-app-title" content="UchiStoke" />
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}" />
     <!-- Fonts -->
     {{-- <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
